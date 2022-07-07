6e1eb6a8b636aa4e78d217f7e034c661908895d8
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-07-07 19:26:07 +0300

    redit show done

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -6,21 +6,12 @@
 		<div class="container-fluid">
 			<div class="row mb-2 ">
 				<div class="col-sm-6">
-					<h1 class="m-0">Категории</h1>
+					<h1 class="m-0">Редактирование категории</h1>
+					<h5 class="mt-2">{{$category->title}}</h5>
 				</div>
 			</div>
 		</div>
 	</div>
-	<section class="content ">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm-6 col-md-3">
-					<button class="btn btn-primary  ps-5 pe-5"><a href="{{	route('admin.category.create')	}}" class="text-white">Добавить</a></button>
-				</div>
-			</div>
-		</div>
-	</section>
-
 	<section>
 		<div class="row mt-5">
 			<div class="col-9">
@@ -31,23 +22,15 @@
 								<tr>
 									<th>ID</th>
 									<th>Название</th>
-									<th>Дата</th>
 									<th>Количество постов</th>
-									<th>Редактировать</th>
-									<th>Удалить</th>
 								</tr>
 							</thead>
 							<tbody>
-								@foreach ($categories as $category)
 								<tr>
 									<td>{{$category->id}}</td>
 									<td>{{$category->title}}</td>
-									<td>{{$category->created_at}}</td>
 									<td><span class="tag tag-success">Потом будет к каким постам привязано</span></td>
-									<td><a href="{{route('admin.category.show', $category->id)}}"><span style="color:white;"><i class="fas fa-pen"></i></span></a></td>
-									<td><a href="#"><span style="color:red;"><i class="fa-solid fa-trash-can"></i></span></a></td>
 								</tr>
-								@endforeach
 							</tbody>
 						</table>
 					</div>
